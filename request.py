566efc8bfcc836b9e1c38c3cf865dd15785db6f9
hjpython <834775954@qq.com>
2017-11-21 00:59:16 +0800

    request基础爬取网页代码

--- request.py
+++ request.py
@@ -7,6 +7,7 @@ dataline = file.readline()
 fhandle = open('/home/hj/pachong/1.html','wb')
 fhandle.write(data)
 fhandle.close()
+
 filename = urllib.request.urlretrieve('http://edu.51cto.com',filename = '/home/hj/pachong/2.html')
 urllib.request.urlcleanup()
 print(file.info())
