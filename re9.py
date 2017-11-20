73a6bb43045e6958509336303e7d94d7a88fbb8a
hjpython <834775954@qq.com>
2017-11-20 14:34:21 +0800

    模式单元符

---
+++ re9.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+import re
+pattern1 = '(cd){1,}'
+pattern2 = 'cd{1,}'
+string = 'abcdcdcdcdfphp345pythony_py'
+result1 = re.search(pattern1,string)
+result2 = re.search(pattern2,string)
+print(result1)
+print(result2)
