06f8fe0c4c8ef62ccba217749a778b5e2b841953
hjpython <834775954@qq.com>
2017-11-21 14:06:01 +0800

    url编码

--- quote.py
+++ quote.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 import urllib.request
-url = 'http://blog.csdn.net/weiwei_pig/article/details/51178226'
-file = urllib.request.urlopen(url)
-data = file.read()
-fhandle =  open('/home/hj/python/pythoncode/url/1.html','wb')
-fhandle.write(data)
-fhandle.close()
+url = urllib.request.quote('http://www.sina.com.cn')
+result = urllib.request.unquote(url)
+print(result)
