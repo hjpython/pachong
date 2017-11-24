e5e56a79440cec60687daa9b6a8bc5a54b04e4d2
hjpython <834775954@qq.com>
2017-11-24 13:26:09 +0800

    中文输出与存储

---
+++ mypjt/mypjt/pipelines.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import codecs
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class MypjtPipeline(object):
+    def __init__(self):
+        self.file = codecs.open('/home/hj/pachong/mydata1.txt','wb',encoding='utf-8')
+    def process_item(self, item, spider):
+        l = str(item) + '\n'
+        print(l)
+        self.file.write(l)
+        return item
+    def close_spider(self,spider):
+        self.file.close()
---
+++ mypjt/mypjt/spiders/weisuen.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from mypjt.items import MypjtItem
+
+class WeisuenSpider(scrapy.Spider):
+    name = 'weisuen'
+    allowed_domains = ['sina.com.cn']
+    start_urls = ('http://tech.sina.com.cn/d/s/2016-09-17/doc-ifxvyqwa3324638.shtml',)
+
+    def parse(self, response):
+        item = MypjtItem()
+        item['title'] = response.xpath('/html/head/title/text()')
+        print(item['title'])
