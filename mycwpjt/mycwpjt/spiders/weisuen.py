5d682baf60902b9d723a94900c34d14e3a9fac97
hjpython <834775954@qq.com>
2017-11-25 12:21:00 +0800

    自动爬虫

---
+++ mycwpjt/mycwpjt/spiders/weisuen.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from mycwpjt.items import MycwpjtItem
+class WeisuenSpider(CrawlSpider):
+    name = 'weisuen'
+    allowed_domains = ['sohu.com']
+    start_urls = ['http://sohu.com/']
+
+    rules = (
+        Rule(LinkExtractor(allow=('.*?/n.*?shtml'),allow_domains=('sohu.com'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        i = MycwpjtItem()
+        i['name'] = response.xpath('/html/head/title/text()').extract()
+        i['link'] = response.xpath('//link[@rel="canonical"]/@href').extract()
+        return i
