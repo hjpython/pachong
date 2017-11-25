4418cb2cdad40800b6b59d21a214207b3818784d
hjpython <834775954@qq.com>
2017-11-25 18:15:53 +0800

    scrapy爬取图片爬虫

---
+++ qtpjt/qtpjt/spiders/qtspd.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re 
+from qtpjt.items import QtpjtItem
+from scrapy.http import Request
+class QtspdSpider(scrapy.Spider):
+    name = 'qtspd'
+    allowed_domains = ['58pic.com']
+    start_urls = ['http://www.58pic.com/piccate/3-8-0.html',]
+
+    def parse(self, response):
+        item = QtpjtItem()
+        paturl = '(http://pic.qiantucdn.com/58pic/.*?).jpg'
+        item['picurl'] = re.compile(paturl).findall(str(response.body))
+        patlocal = 'http://pic.qiantucdn.com/58pic/.*?/.*?/.*?/(.*?).jpg'
+        item['picid'] = re.compile(patlocal).findall(str(response.body))
+        yield item
+        for i in range(21,21):
+            nexturl = 'http://www.58pic.com/piccate/3-8-0-'+str(i)+'.html'
+            yield Request(nexturl,callback=self.parse)
