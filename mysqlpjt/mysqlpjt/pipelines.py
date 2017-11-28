5132788d2efbf86b3b2c0e292860dbd7bc8812ed
hjpython <834775954@qq.com>
2017-11-28 14:29:09 +0800

    mysql

--- mysqlpjt/mysqlpjt/pipelines.py
+++ mysqlpjt/mysqlpjt/pipelines.py
@@ -8,12 +8,13 @@
 import pymysql
 class MysqlpjtPipeline(object):
     def __inti__(self):
-        self.conn = pymysql.connect(host='192.168.32.1',user='root',passwd='123456',db=mypydb)
+        self.conn = pymysql.connect(host='192.168.32.1',user='root',passwd='123456',db="mypydb")
     def process_item(self, item, spider):
         name = item['name'][0]
         key = item['keywd'][0]
-        sql = 'insert into mytb(title,keywd) values('"+name+"','"+key+"')'
+        sql = "insert into mytb(title,keywd) VALUES('"+name+"','"+key+"')"
         self.conn.query(sql)
+        self.conn.commit()
         return item
     def close_spider(self,spider):
         self.conn.close()
